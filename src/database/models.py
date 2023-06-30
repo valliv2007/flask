47dff5e29f49cf141cb667b2153fe1ae736dd7a4
Илья Снежко <valliv2007@ya.ru>
2023-06-30 12:51:46 +0300

    create travis config

--- src/database/models.py
+++ src/database/models.py
@@ -90,7 +90,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50),  unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(50),  unique=True, nullable=False)
     password = db.Column(db.String(254), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
